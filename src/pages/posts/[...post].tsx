de22a37710c6638fb9647abf21f4a1577b495afb
QuangDo <do.quang039@gmail.com>
2023-10-11 16:42:14 +0700

    remove same name file

--- src/pages/posts/[...post].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-const Post = () => {
-  const router = useRouter();
-  console.log("router", router);
-  return <div>{router.query.post}</div>;
-};
-
-export default Post;
--- src/pages/posts/[[...post]].tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-const PostOptional = () => {
-  const router = useRouter();
-  return <div>{router.query.post}</div>;
-};
-
-export default PostOptional;
